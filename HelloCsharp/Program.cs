70bdfcf763b1cbbcc9c2c76a775b5c678dbf71a2
gurumee92 <gurumee92@woowahan.com>
2024-08-15 23:00:43 +0900

    feat: Add HelloCsharp Project

---
+++ HelloCsharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloCsharp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
