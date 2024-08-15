5c7812df4ff2a0c62b3d10e381178306bcca5077
gurumee92 <gurumee92@woowahan.com>
2024-08-15 23:38:05 +0900

    learn: ch03-02

--- HelloCsharp/Program.cs
+++ HelloCsharp/Program.cs
@@ -1,8 +1,35 @@
-﻿namespace HelloCsharp;
-class Program
+﻿namespace HelloCsharp
 {
-    static void Main(string[] args)
+    class GiftBox
     {
-        Console.WriteLine("Hello C#!");
+        public required string Letter;
+        public int Money;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GiftBox a = new()
+            {
+                Letter = "A 친구야 잘 지내니?",
+                Money = 100_000,
+            };
+            GiftBox b = new()
+            {
+                Letter = "B 친구야 잘 지내니?",
+                Money = 120_000,
+            };
+            GiftBox c = new()
+            {
+                Letter = "C 친구야 잘 지내니?",
+                Money = 130_000,
+            };
+            Console.WriteLine($"a 내용: {a.Letter}\n돈: {a.Money}");
+            Console.WriteLine($"b 내용: {b.Letter}\n돈: {b.Money}");
+            Console.WriteLine($"c 내용: {c.Letter}\n돈: {c.Money}");
+        }
     }
 }
+
+
